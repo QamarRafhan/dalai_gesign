ddd89a65a7a2d230ac0a9a06eebc45e1d0c9f447
“Qamar <qamarrafhan@gmail.com>
2023-02-22 18:11:16 +0500

    Wip

--- resources/views/request/create-modal.blade.php
+++ resources/views/request/create-modal.blade.php
@@ -7,38 +7,29 @@
                     <span aria-hidden="true">×</span>
                 </button>
             </div>
-            <form id="user-delete-form" method="POST" action="{{route('funds.store')}}">
+            <form id="request-form" method="POST" action="">
                 @csrf
                 <div class="modal-body">
                     <div class="form-group row">
                         <div class="col-12 mb-3">
                             <span style="color:red;">*</span>Amount</label>
-                            <input type="number" step="0.1" class="form-control form-control-user @error('name') is-invalid @enderror" placeholder="Amounts" name="name" value="{{ old('name') }}">
+                            <input type="number" step="0.1" class="form-control form-control-user @error('amount') is-invalid @enderror" placeholder="Amounts" name="amount" value="{{ old('amount') }}">
                         </div>
                         <div class="col-12 mb-3">
                             <span style="color:red;">*</span>Amount Eur</label>
-                            <input type="number" step="0.1" class="form-control form-control-user @error('name') is-invalid @enderror" placeholder="Amount Eur" name="amount_eur" value="{{ old('name') }}">
+                            <input type="number" step="0.1" class="form-control form-control-user @error('amount_eur') is-invalid @enderror" placeholder="Amount Eur" name="amount_eur" value="{{ old('amount_eur') }}">
                         </div>
                         <div class="col-12 mb-3">
-                            <span style="color:red;">*</span>Amount</label>
-                            <input type="date" class="form-control form-control-user @error('name') is-invalid @enderror" placeholder="Date" name="date" value="{{ old('date') }}">
+                            <span style="color:red;">*</span>Date</label>
+                            <input type="date" class="form-control form-control-user @error('date') is-invalid @enderror" placeholder="Date" name="date" value="{{ old('date') }}">
                         </div>
 
 
                         <div class="col-12 mb-3">
-                            <span style="color:red;">*</span>Fund
-                            <select class="form-control form-control-user " name="status" required>
-                                <option value="0">Requested</option>
-                                <option value="1">Accepted</option>
-                                <option value="2">Completed</option>
-                            </select>
-                        </div>
-                        <div class="col-12 mb-3">
-                            <span style="color:red;">*</span>User
-                            <select class="form-control form-control-user " name="status" required>
-                                <option value="0">Requested</option>
-                                <option value="1">Accepted</option>
-                                <option value="2">Completed</option>
+                            <span style="color:red;">*</span>Operacion Type
+                            <select class="form-control form-control-user " name="operacion_type" required>
+                                <option value="0">Out</option>
+                                <option value="1">In </option>
                             </select>
                         </div>
                         <div class="col-12 mb-3">
@@ -54,10 +45,28 @@
                 <div class="modal-footer">
 
                     <button class="btn btn-primary float-right mr-3 mb-3">Cancel</button>
-                    <button type="submit" class="btn btn-success btn-user float-right mb-3"> Save</button>
+                    <button type="submit" class="btn btn-success btn-user float-right mb-3" id="form_submit"> Save</button>
 
                 </div>
             </form>
         </div>
     </div>
 </div>
+<script>
+   $('#form_submit').submit(function(event){
+      event.preventDefault();
+      alert('test');
+//    $.ajax({
+    //   type:"post",
+    
+//       dataType="json",
+//       data:$('#request-form').serialize(),
+//       success: function(data){
+//          alert("Data Save: " + data);
+//       }
+//       error: function(data){
+//          alert("Error")
+//       }
+//    });
+   });
+</script>
